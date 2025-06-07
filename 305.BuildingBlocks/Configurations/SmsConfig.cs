cbe0ebef513bf20a56749e51db3fbe34a9d76b46
Uncle Bamdad <mohammadJavadTabari1024@outlook.com>
2025-06-07 11:28:33 -0700

    Final refactor for5 infrastructure

---
+++ 305.BuildingBlocks/Configurations/SmsConfig.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _305.BuildingBlocks.Configurations;
+public static class SmsConfig
+{
+	public const string ApiKey = "7762575A6D727561537779314251716C6B2B6C444B6E38316445357A6B3143306D634C42316A7A2F546C4D3D";
+	public const string SenderNumber = "9982003589";
+}
